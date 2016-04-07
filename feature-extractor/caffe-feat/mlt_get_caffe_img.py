ea8ed4abf5bad406c0c1ea4130105ace1c75366a
Shubham <agshubh191@gmail.com>
2016-04-07 18:51:37 +0530

    That 0 was correct

--- feature-extractor/caffe-feat/mlt_get_caffe_img.py
+++ feature-extractor/caffe-feat/mlt_get_caffe_img.py
@@ -1,12 +1,12 @@
 import os
-rootdir = '../../data'
+rootdir = '../../data/'
 out_img = open('mlt_allimg.txt','wb')
 out_label = open('mlt_allimg_labels.txt','wb')
+# This 0 is correct ! 
 j = 0
 for dirpath, dirnames, filenames in os.walk(rootdir):
     for file in filenames:
         file= os.path.join(os.path.abspath(dirpath), file)
-        label = os.path.basename(os.path.normpath(os.path.abspath(dirpath)))
         out_img.write(file+'\n')
         out_label.write(str(j)+'\n')
     j += 1
